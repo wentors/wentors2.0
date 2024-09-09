f90334901b3cafb873d7cc1bfc87e56096131ba4
Hero <hero4barca@gmail.com>
2024-09-09 11:15:35 +0100

    added SubscriptionPlan to members.models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class SubscriptionPlan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.DecimalField(decimal_places=2, max_digits=7)
+    months = models.IntegerField()
+    description = models.TextField()
